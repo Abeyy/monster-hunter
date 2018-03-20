6d7c20b055fb9794536e40f2e1580001e374b36a
Abey Joy <abey.joy@nyu.edu>
2018-03-19 22:35:03 -0400

    Add health, healing, taking damage, and basic status (alive vs dead)

--- src/components/hero/hero.vue
+++ src/components/hero/hero.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hero">
     {{ health }}
+    {{ status }}
+    <button v-on:click="incrementHealth(10)">Heal</button>
+    <button v-on:click="decrementHealth(10)">Take Damage</button>
   </div>
 </template>
 
@@ -8,9 +11,34 @@
 export default {
   data() {
     return {
+        status: "alive",
         health: 100
     }
+  },
+  mounted: {
+
+  },
+  components: {
+
+  },
+  computed: {
+
+  },
+  methods: {
+    incrementHealth(val) {
+      this.health = this.health + val
+    },
+    decrementHealth(val) {
+      if (this.status == "dead") return
+      if (this.status == "alive") {
+        this.health = this.health - val
+        if (this.health <= 0) {
+          this.status = 'dead'
+        }
+      }
+    }
   }
+
 }
 </script>
 
