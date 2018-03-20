d67e272c96d5464338002ed5435607780acd65b6
Abey Joy <abey.joy@nyu.edu>
2018-03-19 23:16:39 -0400

    Poison working!

--- src/components/hero/hero.vue
+++ src/components/hero/hero.vue
@@ -4,6 +4,7 @@
     {{ status }}
     <button v-on:click="incrementHealth(10)">Heal</button>
     <button v-on:click="decrementHealth(10)">Take Damage</button>
+    <button v-on:click="togglePoison">Poison</button>
   </div>
 </template>
 
@@ -27,11 +28,38 @@ export default {
     },
     decrementHealth(val) {
       if (this.status == "dead") return
+
+      this.health = this.health - val
+      if (this.health <= 0) {
+        this.status = 'dead'
+      }
+    },
+    togglePoison(val) {
       if (this.status == "alive") {
-        this.health = this.health - val
-        if (this.health <= 0) {
-          this.status = 'dead'
-        }
+        return this.status = "poisoned"
+      }
+
+      return this.status = "alive"
+    }
+  },
+  watch: {
+    status() {
+      let self = this
+      if(this.status == "poisoned") {
+        let poisonCount = setInterval(function (){
+          self.health = self.health - 1
+          if (self.status == "dead" || self.health <= 0) {
+            clearInterval(poisonCount)
+          }
+        }, 1000)
+      }
+    },
+    health() {
+      if (this.health <= 0) {
+        this.status = "dead"
+      }
+      if (this.health > 0) {
+        this.status = "alive"
       }
     }
   }
