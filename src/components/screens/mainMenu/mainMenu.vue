59ecb4fa758f02a554c6f1675c1358f6dbcae42e
Abey Joy <abey.joy@nyu.edu>
2018-03-25 22:48:43 -0400

    More work on main menu!

--- src/components/screens/mainMenu/mainMenu.vue
+++ src/components/screens/mainMenu/mainMenu.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="main-menu">
-    <h1> Monster Hunter </h1>
-    <button v-on:click="changeView('battleground')"> Play Now </button>
+    <div class="space">
+
+    </div>
+    <img class="main-title" src="../../../assets/Monster-Hunter.png">
+    <button v-on:click="changeView('battleground')" class="play-button">
+      <img class="main-title" src="../../../assets/Play-Now.png">
+    </button>
   </div>
 </template>
 
@@ -17,4 +22,34 @@ export default {
 
 <style lang="scss">
 
+  .main-menu {
+    background: url('../../../assets/mainBackground.gif') no-repeat center center fixed;
+    background-size: cover;
+    height: 100vh;
+  }
+
+  .space {
+    height: 100px
+  }
+
+  .main-title, .play-button {
+    display: block;
+    // margin-top: 50px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+  }
+
+  .play-button {
+    width: 40%;
+    background: transparent;
+    border: none;
+    margin-top: 150px;
+    img {
+      cursor: pointer;
+      &:hover {
+        filter: brightness(1.5)
+      }
+    }
+  }
 </style>
